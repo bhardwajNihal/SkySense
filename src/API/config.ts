69307bad97256aecf8679a426c11d6b5b498f147
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2025-03-24 17:01:22 +0530

    fixed url issue

--- src/API/config.ts
+++ src/API/config.ts
@@ -2,7 +2,7 @@
 
 export const API_CONFIGS = {
     BASE_URL : "https://api.openweathermap.org/data/2.5",   //for weather data
-    GEO : "http://api.openweathermap.org/geo/1.0",         //get latitude and longitude based on cityname, helps segregate cities with same name
+    GEO : "https://api.openweathermap.org/geo/1.0",         //get latitude and longitude based on cityname, helps segregate cities with same name
     API_KEY : import.meta.env.VITE_OPENWEATHER_API_KEY,
     DEFAULT_PARAMS : {                                     //params to used in each url during fetch call
         units : "metric",                                  // metric for degree C, imperial for deg F
